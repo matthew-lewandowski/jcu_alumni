e47bdaa0f33c2faf1a55076d0a460872f816516f
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-06-22 20:46:03 +1000

    Make the site logo, and header responsive.

--- header.php
+++ header.php
@@ -25,37 +25,43 @@
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'jcu_alumni'); ?></a>
 
     <header id="masthead" class="site-header" role="banner">
-        <div class="site-branding">
-            <?php
-            the_custom_logo();
-            if (is_front_page() && is_home()) :
-                ?>
-                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                          rel="home"><?php bloginfo('name'); ?></a></h1>
-            <?php
-            else :
-                ?>
-                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                         rel="home"><?php bloginfo('name'); ?></a></p>
-            <?php
-            endif;
-            $jcu_alumni_description = get_bloginfo('description', 'display');
-            if ($jcu_alumni_description || is_customize_preview()) :
-                ?>
-                <p class="site-description"><?php echo $jcu_alumni_description; /* WPCS: xss ok. */ ?></p>
-            <?php endif; ?>
-        </div><!-- .site-branding -->
+        <div class="site-header-restrainer">
+            <div class="site-header__container">
+                <div class="site-branding">
+                    <?php the_custom_logo(); ?>
+                    <div class='site-branding__text'>
+                        <?php
+                        if (is_front_page() && is_home()) :
+                            ?>
+                            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                      rel="home"><?php bloginfo('name'); ?></a></h1>
+                        <?php
+                        else :
+                            ?>
+                            <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                     rel="home"><?php bloginfo('name'); ?></a></p>
+                        <?php
+                        endif;
+                        $jcu_alumni_description = get_bloginfo('description', 'display');
+                        if ($jcu_alumni_description || is_customize_preview()) :
+                            ?>
+                            <p class="site-description"><?php echo $jcu_alumni_description; /* WPCS: xss ok. */ ?></p>
+                        <?php endif; ?>
+                    </div><!-- .site-branding__text -->
+                </div><!-- .site-branding -->
 
-        <nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu"
-                    aria-expanded="false"><?php esc_html_e('Primary Menu', 'jcu_alumni'); ?></button>
-            <?php
-            wp_nav_menu(array(
-                'theme_location' => 'menu-1',
-                'menu_id' => 'primary-menu',
-            ));
-            ?>
-        </nav><!-- #site-navigation -->
+                <nav id="site-navigation" class="main-navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu"
+                            aria-expanded="false"><?php esc_html_e('Primary Menu', 'jcu_alumni'); ?></button>
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'menu-1',
+                        'menu_id' => 'primary-menu',
+                    ));
+                    ?>
+                </nav><!-- #site-navigation -->
+            </div><!-- #site-header__container -->
+        </div><!-- #site-header-restrainer -->
     </header><!-- #masthead -->
     <?php if (get_header_image() && is_front_page()) : //this will only display header image on front page ?>
         <figure class="header-image">
