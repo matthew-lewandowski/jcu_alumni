6f4ff99578c9bd2692ca684ec1933ddcf90c9018
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-07-10 09:35:12 -0500

    Add single Post without sidebar functionaliity

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -34,7 +34,7 @@ if (!function_exists('jcu_alumni_posted_on')) :
         echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
         if (!post_password_required() && (comments_open() || get_comments_number())) {
-            echo ' <span class="comments-link">';
+            echo ' <span class="comments-link"><span class="extra">Discussion </span>';
             comments_popup_link(
                 sprintf(
                     wp_kses(
@@ -65,7 +65,7 @@ if (!function_exists('jcu_alumni_posted_on')) :
                 ),
                 get_the_title()
             ),
-            ' <span class="edit-link">',
+            ' <span class="edit-link"><span class="extra">Admin </span>',
             '</span>'
         );
     }
