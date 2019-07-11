a610bf471393367eff9d39abd2d579c65c5a5741
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-07-11 12:34:51 -0400

    Add styling and functionality to comments section

--- comments.php
+++ comments.php
@@ -15,61 +15,62 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
-	return;
+if (post_password_required()) {
+    return;
 }
 ?>
 
 <div id="comments" class="comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$jcu_alumni_comment_count = get_comments_number();
-			if ( '1' === $jcu_alumni_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'jcu_alumni' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $jcu_alumni_comment_count, 'comments title', 'jcu_alumni' ) ),
-					number_format_i18n( $jcu_alumni_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
+    <?php
+    // You can start editing here -- including this comment!
+    if (have_comments()) :
+        ?>
+        <h2 class="comments-title">
+            <?php
+            $jcu_alumni_comment_count = get_comments_number();
+            if ('1' === $jcu_alumni_comment_count) {
+                printf(
+                /* translators: 1: title. */
+                //esc_html__('One thought on &ldquo;%1$s&rdquo;', 'jcu_alumni'),
+                    esc_html__('One comment', 'jcu_alumni'),
+                    '<span>' . get_the_title() . '</span>'
+                );
+            } else {
+                printf( // WPCS: XSS OK.
+                /* translators: 1: comment count number, 2: title. */
+                //esc_html(_nx('%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $jcu_alumni_comment_count, 'comments title', 'jcu_alumni')),
+                    esc_html(_nx('%1$s comments', '%1$s comments', $jcu_alumni_comment_count, 'comments title', 'jcu_alumni')),
+                    number_format_i18n($jcu_alumni_comment_count),
+                    '<span>' . get_the_title() . '</span>'
+                );
+            }
+            ?>
+        </h2><!-- .comments-title -->
+    
+        <ol class="comment-list">
+            <?php
+            wp_list_comments(array(
+                'style' => 'ol',
+                'short_ping' => true,
+                'avatar_size' => '96',
+            ));
+            ?>
+        </ol><!-- .comment-list -->
 
-		<?php the_comments_navigation(); ?>
+        <?php
+        the_comments_navigation();
 
-		<ol class="comment-list">
-			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
-			?>
-		</ol><!-- .comment-list -->
+        // If comments are closed and there are comments, let's leave a little note, shall we?
+        if (!comments_open()) :
+            ?>
+            <p class="no-comments"><?php esc_html_e('Comments are closed.', 'jcu_alumni'); ?></p>
+        <?php
+        endif;
 
-		<?php
-		the_comments_navigation();
+    endif; // Check for have_comments().
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'jcu_alumni' ); ?></p>
-			<?php
-		endif;
-
-	endif; // Check for have_comments().
-
-	comment_form();
-	?>
+    comment_form();
+    ?>
 
 </div><!-- #comments -->
