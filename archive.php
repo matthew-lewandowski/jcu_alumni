59131b8fa253be0ab47c4273a7eaf39908f47741
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-07-15 16:09:13 -0500

    Archive title and navigation functionality

--- archive.php
+++ archive.php
@@ -9,44 +9,51 @@
 
 get_header();
 ?>
+<?php if (have_posts()) : ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <header class="page-header">
+        <?php
+        the_archive_title('<h1 class="page-title">', '</h1>');
+        the_archive_description('<div class="archive-description">', '</div>');
+        ?>
+    </header><!-- .page-header -->
+<?php endif; ?>
 
-		<?php if ( have_posts() ) : ?>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+            <?php if (have_posts()) : ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+                <?php
+                /* Start the Loop */
+                while (have_posts()) :
+                    the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                    /*
+                     * Include the Post-Type-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                     */
+                    get_template_part('template-parts/content', get_post_type());
 
-			endwhile;
+                endwhile;
 
-			the_posts_navigation();
+                the_posts_pagination(array(
+                    'prev_text' => __('Newer', 'jcu_alumni'),
+                    'next_text' => __('Older', 'jcu_alumni'),
+                    'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'jcu_alumni' ) . '</span>',
 
-		else :
+                ));
 
-			get_template_part( 'template-parts/content', 'none' );
+            else :
 
-		endif;
-		?>
+                get_template_part('template-parts/content', 'none');
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            endif;
+            ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_sidebar();
