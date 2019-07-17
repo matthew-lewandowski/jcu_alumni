eb524b0f89fc01a1bdc5dcead890c3a62f3f1c48
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-07-16 22:29:54 -0500

    Footer nav icons and top menu functionality

--- functions.php
+++ functions.php
@@ -230,6 +230,15 @@ function jcu_alumni_scripts()
 add_action('wp_enqueue_scripts', 'jcu_alumni_scripts');
 
 /**
+ * SVG Icons class.
+ */
+require get_template_directory() . '/classes/class-jcu_alumni-svg-icons.php';
+
+/**
+ * Custom Comment Walker template.
+ */
+require get_template_directory() . '/classes/class-jcu_alumni-walker-comment.php';
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
