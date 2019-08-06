7a727f7c78f6771c1d029ad142a858ef3970f8ee
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-08-06 12:59:26 +1000

    Styling to custom search bar along with bug fixes.

--- js/map-widget.js
+++ js/map-widget.js
@@ -13,12 +13,17 @@ function clickedChild(i) {
  * @param i is the parent list item
  */
 function clickedParent(i) {
+    var parents = document.getElementsByClassName("parentUL");
     var category = i.innerHTML;
-    var child = i.children[1];
-    var symbol = i.children[0];
-    // i.innerHTML = category;
+    var child = i.parentElement.children[2];
+    var symbol = i.parentElement.children[0];
     
     if (!child.classList.contains("show")) {
+        for (var i = 0; i < parents.length; i++) {
+            parents[i].children[2].classList.remove("show");
+            parents[i].children[0].classList.remove("fa-minus");
+            parents[i].children[0].classList.add("fa-plus");
+        }
         child.classList.add("show");
         symbol.classList.remove("fa-plus")
         symbol.classList.add("fa-minus")
@@ -28,4 +33,5 @@ function clickedParent(i) {
         symbol.classList.remove("fa-minus")
         symbol.classList.add("fa-plus")
     }
+
 }
--- template-parts/content-map.php
+++ template-parts/content-map.php
@@ -23,6 +23,9 @@
         <div class="post-content__wrap">
             <aside id="page-secondary" class="widget-area page-sidebar">
                 <div id="map-side">
+                    <div>
+                        <p>Search By</p>
+                    </div>
                     <?php
 
                     function hierarchical_category_tree($cat)
@@ -39,7 +42,7 @@
                                 if ($cat->category_parent > 0) {
                                     echo '<ul class="childUL"><li class="child" onclick="clickedChild(this)"><p>' . $cat->name . '</p>';
                                 } else {
-                                    echo '<ul onclick="clickedParent(this)"><i id="symbol" class="fa fa-plus" aria-hidden="true"></i>' . $cat->name . '<li class="parent">';
+                                    echo '<ul class="parentUL"><i onclick="clickedParent(this)" id="symbol" class="fa fa-plus" aria-hidden="true"></i><p  onclick="clickedParent(this)">' . $cat->name . '</p><li class="parent">';
                                 }
 //                                echo '<ul><li onclick="clicked(this, ' . $cat->name . ')">' . $cat->name . '</li>';
 
