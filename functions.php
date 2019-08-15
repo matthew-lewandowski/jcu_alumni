0e13c48aad453bb5bdf659eaa9ac8e4819bbc9f9
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-08-15 12:25:31 +1000

    fixed cropping issue

--- functions.php
+++ functions.php
@@ -151,15 +151,15 @@ add_filter('wp_resource_hints', 'jcu_alumni_resource_hints', 10, 2);
  *
  * @global int $content_width
  */
-function jcu_alumni_content_width()
-{
-    // This variable is intended to be overruled from themes.
-    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-    $GLOBALS['content_width'] = apply_filters('jcu_alumni_content_width', 640);
-}
-
-add_action('after_setup_theme', 'jcu_alumni_content_width', 0);
+//function jcu_alumni_content_width()
+//{
+//    // This variable is intended to be overruled from themes.
+//    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+//    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+//    $GLOBALS['content_width'] = apply_filters('jcu_alumni_content_width', 640);
+//}
+//
+//add_action('after_setup_theme', 'jcu_alumni_content_width', 0);
 
 /**
  * Register widget area.
